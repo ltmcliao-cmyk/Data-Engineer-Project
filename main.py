2917cd4103da02d7db1575e93e73e358eb98bc7a
lutomica <ltmc.liao@gmail.com>
2026-03-29 21:57:40 +0800

    test docker command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from de-project!")
+import datetime
 
+def run():
+    now = datetime.datetime.now()
+    print(f"[{now}] 恭喜！Hao-Chun 的雲端流水線執行成功。")
 
 if __name__ == "__main__":
-    main()
+    run()
